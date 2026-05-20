955ac1c8a9fcdbdf17f7ca297927549785646168
u-operator <harizhakim@gmail.com>
2026-05-20 13:34:50 +0800

    Added new states

--- fsm.py
+++ fsm.py
@@ -4,6 +4,8 @@ from idlelib.config import IdleConf
 
 
 class State(Enum):
+    GO_TO_LINE = auto()
+    WAIT_TO_START = auto()
     IDLE = auto()
     NAVIGATE_TO_PICK = auto()
     SCAN_CUBE = auto()
@@ -19,7 +21,8 @@ PHASE1_DURATION = 180 # 3minutes
 class Phase1FSM:
     def __init__(self, robot):
         self.robot = robot
-        self.state = State.IDLE
+        self.cur_state = State.WAIT_TO_START
+        self.prev_state = None
         self.detected_label = None
         self.start_time = None
 
@@ -27,43 +30,50 @@ class Phase1FSM:
         return PHASE1_DURATION - (time.time() - self.start_time)
 
     def run(self):
+        attempt = 1
         while True:
 
             if self.start_time and self.time_remaining() <= 0:
-                self.state = State.RETURN_TO_START
-
-            self.step()
+                self.cur_state = State.RETURN_TO_START
+                
+            
 
     def step(self):
-        match self.state:
-
-            case State.IDLE:
+        match self.cur_state:
+            case State.WAIT_TO_START: # Beginning state or first attempt
                 self.robot.wait_to_start()
                 self.start_time = time.time()
-                self.state = State.NAVIGATE_TO_PICK
+                self.cur_state = State.NAVIGATE_TO_PICK
+                
+            case State.IDLE: # When waiting to reattempt, this is the state
+                self.robot.wait_to_start()
+                self.cur_state = State.NAVIGATE_TO_PICK
 
             case State.NAVIGATE_TO_PICK:
-                success = self.robot.navigate("start zone", "pick zone")
+                if self.cur_state is State.IDLE or State.WAIT_TO_START:  # Nav to pick from start
+                   success = self.robot.navigate("start zone", "pick zone")
+                elif self.prev_state is State.DEPOSIT:
+                   success = self.robot.navigate('storing zone', 'pick zone') # Nav to pick from store zone
                 if success:
-                    self.state = State.SCAN_CUBE
+                    self.cur_state = State.SCAN_CUBE
                 else:
-                    self.state = State.IDLE
+                    self.cur_state = State.IDLE
 
             case State.SCAN_CUBE:
                 if not self.robot.cubes_remaining:
-                    self.state = State.RETURN_TO_START
+                    self.cur_state = State.RETURN_TO_START
                     return
 
                 self.detected_label = self.robot.scan_cube()
 
                 if self.detected_label in REAL_CUBES:
-                    self.state = State.PICK_CUBE
+                    self.cur_state = State.PICK_CUBE
                 else:
-                    self.state = State.DISPLACE_FAKE
+                    self.cur_state = State.DISPLACE_FAKE
 
             case State.PICK_CUBE:
                 self.robot.pick_cube()
-                self.state = State.NAVIGATE_TO_STORE
+                self.cur_state = State.NAVIGATE_TO_STORE
 
             case State.DISPLACE_FAKE:
                 pass
@@ -71,15 +81,29 @@ class Phase1FSM:
             case State.NAVIGATE_TO_STORE:
                 success = self.robot.navigate("pick zone", "storing zone")
                 if success:
-                    self.state = State.DEPOSIT
+                    self.cur_state = State.DEPOSIT
                 else:
-                    self.state = State.IDLE
+                    self.cur_state = State.IDLE
 
             case State.DEPOSIT:
                 self.robot.deposit_cube()
 
                 if self.time_remaining() > 10:
-                    self.state = State.NAVIGATE_TO_PICK
+                    self.cur_state = State.NAVIGATE_TO_PICK
                 else:
-                    self.state = State.RETURN_TO_START
+                    self.cur_state = State.RETURN_TO_START
 
+            case State.GO_TO_LINE: # Intermediate state between navigating the field
+                match self.prev_state:
+                    case State.IDLE | State.IDLE.WAIT_TO_START:
+                        if not self.robot.line_follow.creep_until_line():
+                            self.cur_state = State.IDLE
+                        self.cur_state = State.NAVIGATE_TO_PICK
+
+                    case State.DEPOSIT:
+                        pass
+                        # self.cur_state = State.NAVIGATE_TO_PICK
+
+                    case State.PICK_CUBE:
+                        pass
+                        # self.cur_state = State.NAVIGATE_TO_STORE
